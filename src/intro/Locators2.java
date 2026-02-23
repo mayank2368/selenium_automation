579c96e507febcf3be9032404610352e905b8f20
Mayank-Admin <mayank-admin@MacBookAir.bbrouter>
2026-02-24 00:46:11 +0100

    Assertion example with string concatenation for dynamic data

--- src/intro/Locators2.java
+++ src/intro/Locators2.java
@@ -9,18 +9,22 @@ import org.testng.Assert;
 
 public class Locators2 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		WebDriver driver = new ChromeDriver();
+		String name = "rahul";
 		System.setProperty("webdriver.chrome.driver", "/Users/mayank-admin/selenium-automation /chromedriver");
 		// driver.get("https://www.google.com");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
-		driver.findElement(By.id("inputUsername")).sendKeys("mayank");
-		driver.findElement(By.name("inputPassword")).sendKeys("hello123");
+		driver.findElement(By.id("inputUsername")).sendKeys("rahul");
+		driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
 		driver.findElement(By.className("signInBtn")).click();
+		Thread.sleep(2000);
 		System.out.println(driver.findElement(By.tagName("p")).getText());
-		Assert.ass
+		Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "You are successfully logged in.");
+		Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText(),
+				"Hello " + name + ",");
 
 	}
 
