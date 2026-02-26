0d1b3aee27da93b963787b0e4a365405b0f9fc53
Mayank-Admin <mayank-admin@MacBookAir.bbrouter>
2026-02-26 15:15:17 +0100

    Extract password from htm text using split method

--- src/intro/Locators2.java
+++ src/intro/Locators2.java
@@ -16,9 +16,10 @@ public class Locators2 {
 		System.setProperty("webdriver.chrome.driver", "/Users/mayank-admin/selenium-automation /chromedriver");
 		// driver.get("https://www.google.com");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		String password = getPassword(driver);
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		driver.findElement(By.id("inputUsername")).sendKeys("rahul");
-		driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.name("inputPassword")).sendKeys(password);
 		driver.findElement(By.className("signInBtn")).click();
 		Thread.sleep(2000);
 		System.out.println(driver.findElement(By.tagName("p")).getText());
@@ -27,6 +28,27 @@ public class Locators2 {
 				"Hello " + name + ",");
 		driver.findElement(By.xpath("//*[text()='Log Out']")).click();
 		driver.close();
+
+	}
+
+	public static String getPassword(WebDriver driver) throws InterruptedException {
+
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		driver.findElement(By.linkText("Forgot your password?")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+		String passwordText = driver.findElement(By.cssSelector("form p")).getText();
+		// Please use temporary password 'rahulshettyacademy' to Login.
+		String[] passwordArray = passwordText.split("'");
+		// 0th index - Please use temporary password
+		// 1st index - rahulshettyacademy' to Login.
+		String password = passwordArray[1].split("'")[0];
+		return password;
+		// another long way
+//		String[] passwordArray2 = passwordText[1].split("'");
+//		passwordArray2[0];
+		// 0th index - rahulshettyacademy
+		// 1st index - to Login.
 
 	}
 
