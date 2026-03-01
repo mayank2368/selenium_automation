acc86223a320d79d1db12d35c90d9cdb29913655
Mayank-Admin <mayank-admin@MacBookAir.bbrouter>
2026-03-01 12:26:24 +0100

    Increment number of items using loop

--- src/intro/StaticDropDown.java
+++ src/intro/StaticDropDown.java
@@ -2,25 +2,36 @@ package intro;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class StaticDropDown {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
-		WebElement staticDropdown = driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
-		Select dropdown = new Select(staticDropdown);
-		dropdown.selectByIndex(3);
-		System.out.println(dropdown.getFirstSelectedOption().getText());
-		dropdown.selectByVisibleText("AED");
-		System.out.println(dropdown.getFirstSelectedOption().getText());
-		dropdown.selectByValue("INR");
-		System.out.println(dropdown.getFirstSelectedOption().getText());
+//		WebElement staticDropdown = driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
+//		Select dropdown = new Select(staticDropdown);
+//		dropdown.selectByIndex(3);
+//		System.out.println(dropdown.getFirstSelectedOption().getText());
+//		dropdown.selectByVisibleText("AED");
+//		System.out.println(dropdown.getFirstSelectedOption().getText());
+//		dropdown.selectByValue("INR");
+//		System.out.println(dropdown.getFirstSelectedOption().getText());
+
+		driver.findElement(By.id("divpaxinfo")).click();
+		Thread.sleep(2000);
+//		int i = 1;
+//		while (i < 5) {
+//			driver.findElement(By.id("hrefIncAdt")).click();
+//			i++;
+//		}
+		for (int i = 1; i < 5; i++) {
+			driver.findElement(By.id("hrefIncAdt")).click();
+		}
+		driver.findElement(By.id("btnclosepaxoption")).click();
 		// driver.close();
+
 	}
 
 }
