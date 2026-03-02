ebfbc72e0cfe5aa08dd0173674142bc945facdab
Mayank-Admin <mayank-admin@MacBookAir.bbrouter>
2026-03-02 13:45:55 +0100

    Dynamic dropdown using parent-child relationship xpath

--- src/intro/DynamicDropdown.java
+++ src/intro/DynamicDropdown.java
@@ -11,13 +11,24 @@ public class DynamicDropdown {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 
+//		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+//
+//		driver.findElement(By.xpath("//a[@value='BLR']")).click();
+//		Thread.sleep(2000);
+//
+//		driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
+//		Thread.sleep(2000);
+
+		// Using Parent-Child relationship xpath
 		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
-
-		driver.findElement(By.xpath("//a[@value='BLR']")).click();
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_originStation1_CTNR'] //a[@value='BLR']"))
+				.click();
 		Thread.sleep(2000);
 
-		driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']"))
+				.click();
 		Thread.sleep(2000);
+
 		driver.quit();
 
 	}
